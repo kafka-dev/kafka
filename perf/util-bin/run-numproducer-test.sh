25fde0b6f19805f7a247172f26c55803d641f8c8
cburroughs <chris.burroughs@gmail.com>
2011-03-18 13:19:58 -0400

    Consistent whitespace, formatting, sleeps, etc.

--- perf/util-bin/run-numproducer-test.sh
+++ perf/util-bin/run-numproducer-test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+
 for i in 1 10 20 30 40 50;
 do
- ssh $1 "cd $KAFKA_HOME; ./kafka-server.sh server.properties 2>&1 >kafka.out"& 
- sleep 60
- ../run-simulator.sh -kafkaServer=$1 -numTopic=10  -reportFile=$2 -time=15 -numProducer=$i -numConsumer=20 -xaxis=numProducer
- ssh $1 "cd $KAFKA_HOME; ./stop-server.sh"&
- sleep 60
- ssh $1 "rm -rf /tmp/kafka-logs"&
- sleep 200
+    ssh $1 "cd $KAFKA_HOME; ./kafka-server.sh server.properties 2>&1 >kafka.out" &
+    sleep 60
+    ./run-simulator.sh -kafkaServer=$1 -numTopic=10  -reportFile=$2 -time=15 -numProducer=$i -numConsumer=20 -xaxis=numProducer
+    ssh $1 "cd $KAFKA_HOME; ./stop-server.sh" &
+    sleep 20
+    ssh $1 "rm -rf /tmp/kafka-logs" &
+    sleep 40
 done
--- perf/util-bin/run-numtopic-test.sh
+++ perf/util-bin/run-numtopic-test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+
 for i in 1 10 20 30 40 50;
 do
- ssh $1 "cd $KAFKA_HOME; ./kafka-server.sh server.properties 2>&1 >kafka.out"& 
- sleep 60
- ../run-simulator.sh -kafkaServer=$1 -numTopic=$i  -reportFile=$2 -time=15 -numConsumer=20 -numProducer=40 -xaxis=numTopic
- ssh $1 "cd $KAFKA_HOME; ./stop-server.sh"&
- ssh $1 "rm -rf /tmp/kafka-logs"&
- sleep 300
+    ssh $1 "cd $KAFKA_HOME; ./kafka-server.sh server.properties 2>&1 >kafka.out" &
+    sleep 60
+    ./run-simulator.sh -kafkaServer=$1 -numTopic=$i  -reportFile=$2 -time=15 -numConsumer=20 -numProducer=40 -xaxis=numTopic
+    ssh $1 "cd $KAFKA_HOME; ./stop-server.sh" &
+    sleep 20
+    ssh $1 "rm -rf /tmp/kafka-logs" &
+    sleep 40
 done
