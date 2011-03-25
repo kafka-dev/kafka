416eabcad15986520ac39c4be3e1309ea03663e9
cburroughs <chris.burroughs@gmail.com>
2011-03-25 16:07:28 -0400

    Support for running tests and brokers on separate nodes.

--- perf/util-bin/run-numproducer-single-topic.sh
+++ perf/util-bin/run-numproducer-single-topic.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-. `dirname $0`/remote-kafka-env.sh
-
-REMOTE_KAFKA_HOST=$1 # todo: use this instead of localhost for -kafkaServer=localhost (@ problem)
-REMOTE_SIM_HOST=$2
+REMOTE_KAFKA_LOGIN=$1 # user@host format
+REMOTE_SIM_LOGIN=$2
 TEST_TIME=$3
 REPORT_FILE=$4
 
+. `dirname $0`/remote-kafka-env.sh
 
 for i in 1 `seq -s " " 10 10 50` ;
 do
     kafka_startup
-    ssh $REMOTE_SIM_HOST "$SIMULATOR_SCRIPT -kafkaServer=localhost -numTopic=1  -reportFile=$REPORT_FILE -time=$TEST_TIME -numConsumer=0 -numProducer=$i -xaxis=numProducer"
+    ssh $REMOTE_SIM_LOGIN "$SIMULATOR_SCRIPT -kafkaServer=$KAFKA_SERVER -numTopic=1  -reportFile=$REPORT_FILE -time=$TEST_TIME -numConsumer=0 -numProducer=$i -xaxis=numProducer"
     kafka_cleanup
 done
