40d99b672bdf56a9ccefc94bd853223d39180e74
Amit Bhardwaj <abh23@users.noreply.github.com>
2017-08-11 15:01:54 +0530

    Fix kafka-server stop
    Used egrep to find either kafka or .Kafka in running process.

--- bin/kafka-server-stop.sh
+++ bin/kafka-server-stop.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ps ax | grep -i 'kafka.Kafka' | grep -v grep | awk '{print $1}' | xargs kill -SIGINT
+ps ax | egrep -i 'kafka|.Kafka' | grep -v grep | awk '{print $1}' | xargs kill -SIGINT
